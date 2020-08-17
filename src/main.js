5941a2d4f25f811035ad79f6798364e5f6e79d0f
kazhurka <42044750+kazhurka@users.noreply.github.com>
2020-08-17 12:54:45 +0300

    Добавляет структуры данных и функции отрисовки компонентов на их основе.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import {
   createMainNavTemplate
 } from "./view/main-nav";
+
+import {
+  createMovieInfoTemplate
+} from "./view/movie-cards";
 import {
   createMovieCardTemplate
 } from "./view/movie-cards";
 import {
   createMoviesContainerTemplate
-} from "./view/movie-cards";
-import {
-  createMoviesExtraListTemplate
 } from "./view/movie-cards";
 import {
   createMoviesListTemplate
@@ -26,43 +27,143 @@ import {
   createUserRankTemplate
 } from "./view/user-rank";
 
-const MOVIES_CARD_COUNT = 5;
-const MOVIES_EXTRA_CARD_COUNT = 2;
+import {
+  generateCard
+} from "./mock/card";
 
+import {
+  generateRank
+} from "./mock/rank";
+
+
+const MOVIES_CARD_COUNT = 5;
+
+const generateCards = (quantity) => {
+  let cards = [];
+  for (let i = 0; i < quantity; i++) {
+    cards.push(generateCard());
+  }
+  return cards;
+};
+const cards = generateCards(20);
+let sortedCards = cards;
+let rank = generateRank();
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+
 const siteHeaderElement = document.querySelector(`.header`);
-render(siteHeaderElement, createUserRankTemplate(), `beforeend`);
+render(siteHeaderElement, createUserRankTemplate(rank), `beforeend`);
 
 const siteMainElement = document.querySelector(`.main`);
 render(siteMainElement, createSortTemplate(), `beforeend`);
-render(siteMainElement, createMainNavTemplate(), `beforeend`);
+render(siteMainElement, createMainNavTemplate(rank), `beforeend`);
 render(siteMainElement, createMoviesListTemplate(), `beforeend`);
-
-const siteMoviesSection = document.querySelector(`.films`);
-render(siteMoviesSection, createMoviesExtraListTemplate(), `beforeend`);
-render(siteMoviesSection, createMoviesExtraListTemplate(), `beforeend`);
-
 
 const siteMoviesListElement = document.querySelector(`.films-list`);
 render(siteMoviesListElement, createMoviesContainerTemplate(), `beforeend`);
 render(siteMoviesListElement, createShowMoreButtonTemplate(), `beforeend`);
 
-const siteMoviesContainerElement = siteMoviesListElement.querySelector(`.films-list__container`);
-for (let i = 0; i < MOVIES_CARD_COUNT; i++) {
-  render(siteMoviesContainerElement, createMovieCardTemplate(), `beforeend`);
-}
+const byClickClosePopupHandler = () => {
+  document.querySelector(`.film-details__close`).removeEventListener(`keydown`, byClickClosePopupHandler);
+  document.querySelector(`.film-details__close`).removeEventListener(`click`, byKeyClosePopupHandler);
+  document.querySelector(`.film-details`).remove();
+};
 
-const siteMoviesExtraListElements = document.querySelectorAll(`.films-list--extra`);
-Array.from(siteMoviesExtraListElements).forEach(function (item) {
-  render(item, createMoviesContainerTemplate(), `beforeend`);
-  const siteMoviesExContainerElement = item.querySelector(`.films-list__container`);
-  for (let i = 0; i < MOVIES_EXTRA_CARD_COUNT; i++) {
-    render(siteMoviesExContainerElement, createMovieCardTemplate(), `beforeend`);
+const byKeyClosePopupHandler = (evt) => {
+  if (evt.key === `Escape`) {
+    document.querySelector(`.film-details__close`).removeEventListener(`keydown`, byKeyClosePopupHandler);
+    document.querySelector(`.film-details__close`).removeEventListener(`click`, byClickClosePopupHandler);
+    document.querySelector(`.film-details`).remove();
   }
-});
+
+};
+
+const bodyElement = document.querySelector(`body`);
+const openPopupCardHandler = (evt) => {
+  const cardEls = Array.from(document.querySelectorAll(`.film-card`));
+  let index = cardEls.indexOf(evt.target.parentNode);
+  render(bodyElement, createMovieInfoTemplate(sortedCards[index]), `beforeend`);
+  document.querySelector(`.film-details__close`).addEventListener(`click`, byClickClosePopupHandler);
+  document.addEventListener(`keydown`, byKeyClosePopupHandler);
+};
+
+const siteMoviesContainerElement = siteMoviesListElement.querySelector(`.films-list__container`);
+const renderCards = (cardsData) => {
+  for (let i = 0; i < cardsData.length; i++) {
+    render(siteMoviesContainerElement, createMovieCardTemplate(cardsData[i]), `beforeend`);
+  }
+  document.querySelectorAll(`.film-card img`).forEach((item) => {
+    item.addEventListener(`click`, openPopupCardHandler);
+  });
+  document.querySelectorAll(`.film-card__comments`).forEach((item) => {
+    item.addEventListener(`click`, openPopupCardHandler);
+  });
+};
+
 
 const siteStatisticsContainerElement = document.querySelector(`.footer__statistics`);
-render(siteStatisticsContainerElement, createMovieStatisticsTemplate(), `beforeend`);
+render(siteStatisticsContainerElement, createMovieStatisticsTemplate(cards.length), `beforeend`);
+
+const sortButtonsEls = document.querySelectorAll(`.sort__button`);
+
+const sortCardsByDate = (cardsData) => {
+  let sortedCurrentCards = cardsData.slice();
+  sortedCurrentCards.sort((a, b) => {
+    return b.dateRelease - a.dateRelease;
+  });
+  return sortedCurrentCards;
+};
+
+const sortCardsByRate = (cardsData) => {
+  let sortedCurrentCards = cardsData.slice();
+  sortedCurrentCards.sort((a, b) => {
+    return b.rate - a.rate;
+  });
+  return sortedCurrentCards;
+};
+
+const clearCards = () => {
+  siteMoviesContainerElement.innerHTML = ``;
+};
+
+const clickButtonHandler = (evt) => {
+  let activeButton = document.querySelector(`.sort__button--active`);
+  let index = Array.from(sortButtonsEls).indexOf(evt.target);
+  let indexActive = Array.from(sortButtonsEls).indexOf(activeButton);
+  if (indexActive !== index) {
+    clearCards();
+    sortButtonsEls.forEach((item) => {
+      item.classList.remove(`sort__button--active`);
+    });
+    evt.target.classList.add(`sort__button--active`);
+
+    switch (index) {
+      case 0:
+        sortedCards = cards;
+        break;
+      case 1:
+        sortedCards = sortCardsByDate(cards);
+        break;
+      case 2:
+        sortedCards = sortCardsByRate(cards);
+    }
+    renderCards(sortedCards.slice(0, MOVIES_CARD_COUNT));
+    document.querySelector(`.films-list__show-more`).style.display = `block`;
+  }
+};
+sortButtonsEls.forEach((item) => {
+  item.addEventListener(`click`, clickButtonHandler);
+});
+
+const showMoreHandler = () => {
+  const quantity = siteMoviesContainerElement.querySelectorAll(`.film-card`).length;
+  renderCards(sortedCards.slice(quantity, MOVIES_CARD_COUNT + quantity));
+  if (MOVIES_CARD_COUNT + quantity >= sortedCards.length) {
+    document.querySelector(`.films-list__show-more`).style.display = `none`;
+  }
+};
+
+document.querySelector(`.films-list__show-more`).addEventListener(`click`, showMoreHandler);
+renderCards(sortedCards.slice(0, MOVIES_CARD_COUNT));
---
+++ src/mock/card.js
@@ -0,0 +1,198 @@
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const getRandomElFromArr = (array) => {
+  let randomIndex = getRandomInteger(0, array.length - 1);
+  return array[randomIndex];
+};
+
+const getRandomElsFromArr = (array, randomMin, randomMax) => {
+  let sortedEls = new Set();
+  let elsQuantity = getRandomInteger(randomMin, randomMax);
+  for (let i = 0; i < elsQuantity; i++) {
+    let randomIndex = getRandomInteger(0, array.length - 1);
+    sortedEls.add(array[randomIndex]);
+  }
+  return Array.from(sortedEls);
+};
+
+const generateRandomDate = (start, end) => {
+  let date = new Date(start + Math.random() * (end - start));
+  return date;
+};
+
+const generateDirector = () => {
+  const directors = [
+    `Крис Коламбус`,
+    `Альфонсо Куарон`,
+    `Майк Ньюэлл`
+  ];
+  return getRandomElFromArr(directors);
+};
+const generateNames = () => {
+  const names = [
+    `Дэниэл Рэдклифф`,
+    `Руперт Гринт`,
+    `Эмма Уотсон`,
+    `Мэгги Смит`,
+    `Робби Колтрейн`,
+    `Том Фелтон`,
+    `Мэттью Льюис`,
+    `Джеймс Фелпс`,
+    `Фиона Шоу`,
+    `Ричард Гриффитс`,
+    `Бонни Райт`,
+  ];
+  return getRandomElsFromArr(names, 5, 8);
+};
+
+const generateDuration = () => {
+  return `1h ${getRandomInteger(0, 59)}m`;
+};
+
+const generateTitle = () => {
+  const titles = [
+    `Гарри Поттер и Философский камень`,
+    `Гарри Поттер и узник Азкабана`,
+    `Гарри Поттер и Орден феникса`,
+    `Гарри Поттер и Кубок огня`,
+    `Гарри Поттер и Дары смерти`,
+    `Гарри Поттер и Тайная комната`,
+    `Гарри Поттер и Принц-полукровка`,
+  ];
+
+  return getRandomElFromArr(titles);
+};
+
+const generateDateRealease = () => {
+  return generateRandomDate(0, 1262293200000);
+};
+
+const generateCountry = () => {
+  const countries = [
+    `США`,
+    `Аргентина`,
+    `Великобритания`,
+  ];
+  return getRandomElFromArr(countries);
+};
+
+const generateDescription = () => {
+  const descriptions = [`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+    ` Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit,eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    ` Aliquam erat volutpat.Nunc fermentum tortor ac porta dapibus.`,
+    `In rutrum ac purus sit amet tempus.`,
+  ];
+
+  return getRandomElsFromArr(descriptions, 1, 5).join(` `);
+};
+
+const generatePoster = () => {
+  const posters = [
+    `./images/posters/made-for-each-other.png`,
+    `./images/posters/popeye-meets-sinbad.png`,
+    `./images/posters/sagebrush-trail.jpg`,
+    `./images/posters/santa-claus-conquers-the-martians.jpg`,
+    `./images/posters/the-dance-of-life.jpg`,
+    `./images/posters/the-great-flamarion.jpg`,
+    `./images/posters/the-man-with-the-golden-arm.jpg`,
+  ];
+
+  return getRandomElFromArr(posters);
+};
+
+const generateGenres = () => {
+  const genres = [
+    `фэнтези`,
+    `боевик`,
+    `комедия`,
+  ];
+
+  return getRandomElsFromArr(genres, 1, genres.length);
+};
+
+const generateEmotion = () => {
+  const emotions = [
+    `./images/emoji/angry.png`,
+    `./images/emoji/puke.png`,
+    `./images/emoji/sleeping.png`,
+    `./images/emoji/smile.png`,
+  ];
+  return getRandomElFromArr(emotions);
+};
+
+const generateText = () => {
+  const texts = [
+    `Booooooooooring`,
+    `Almost two hours? Seriously?`,
+    `Interesting setting and a good cast`,
+    `Very very old. Meh`,
+  ];
+  return getRandomElFromArr(texts);
+};
+
+const generateAuthor = () => {
+  const authors = [
+    `John Doe`,
+    `Tim Macoveev`,
+    `Harry Potter`,
+    `Tarzan`,
+  ];
+
+  return getRandomElFromArr(authors);
+};
+const generateDateOfComment = () => {
+  const dates = [
+    `2 days ago`,
+    `Today`,
+    `3 days ago`,
+  ];
+  return getRandomElFromArr(dates);
+};
+const generateComment = () => {
+  return {
+    emotion: generateEmotion(),
+    text: generateText(),
+    author: generateAuthor(),
+    date: generateDateOfComment(),
+  };
+};
+
+const generateComments = () => {
+  let quantity = getRandomInteger(2, 5);
+  let comments = [];
+  for (let i = 0; i < quantity; i++) {
+    comments.push(generateComment());
+  }
+  return comments;
+};
+
+export const generateCard = () => {
+  let title = generateTitle();
+  let commentsData = generateComments();
+  return {
+    cover: generatePoster(),
+    name: title,
+    originalName: title,
+    rate: getRandomInteger(5, 10),
+    director: generateDirector(),
+    actors: generateNames(),
+    writers: generateNames(),
+    dateRelease: generateDateRealease(),
+    duration: generateDuration(),
+    country: generateCountry(),
+    genre: generateGenres(),
+    description: generateDescription(),
+    comments: commentsData,
+    numberOfComments: commentsData.length,
+  };
+};
